2e5ad23a1fde05b3fb06e423f54177cfe3155b40
Vijay Patel <veejspeej@gmail.com>
2017-10-24 18:19:54 +0100

    Make webapp more useful by specifying contract address and user identity.

--- webapp/index.js
+++ webapp/index.js
@@ -1,26 +1,58 @@
 const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-VotingContract = web3.eth.contract(abi);
-contractInstance = VotingContract.at('0xb511274cc89934766ec073b1f44ae7ca5f57f962');
+const VotingContract = web3.eth.contract(abi);
+var contractInstance;
 
 // No apparent way to return arrays from Solidity contract
 const candidateNames = ["Bob", "Tom", "Joe"]
 
+function loadVoteContractState() {
+  contractAddress = $("#contract_address").val()
+  contractInstance = VotingContract.at(contractAddress);
+  for (var i = 0; i < candidateNames.length; i++) {
+    let name = candidateNames[i];
+    let val = contractInstance.totalVotesFor.call(name, function(err, result) {
+      if (err) {
+        alert("Problem fetching vote state: " + err)
+      } else {
+        $("#" + name.toLowerCase()).html(result.toString());
+      }
+    })
+  }
+}
+
 function voteForCandidate() {
+  if (!contractInstance) {
+    alert("You need to enter the contract address first")
+    return
+  }
+
+  const userIdentity = $("#identity").val();
+  if (!userIdentity){
+    alert("You need to enter your identity/public key")
+    return
+  }
+
   const candidateName = $("#candidate").val();
+  if (!candidateName){
+    alert("You need to enter the candidate's name")
+    return
+  }
+
+  // action vote
   contractInstance.voteForCandidate(
     candidateName,
     {
-      from: web3.eth.accounts[0]
+      from: userIdentity
     },
     function() {
       $("#" + candidateName.toLowerCase()).html(contractInstance.totalVotesFor.call(candidateName).toString());
     });
 }
 
+// Load contract/vote state on page ready (if possible)
 $(document).ready(function() {
-  for (var i = 0; i < candidateNames.length; i++) {
-    let name = candidateNames[i];
-    let val = contractInstance.totalVotesFor.call(name).toString()
-    $("#" + name.toLowerCase()).html(val);
+  contractAddress = $("#contract_address").val()
+  if (contractAddress) {
+    loadVoteContractState()
   }
 });
