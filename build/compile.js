427c37c1d1917d1fe8f7149acf1b600ee1830b4d
Vijay Patel <veejspeej@gmail.com>
2017-10-24 17:14:23 +0100

    Changed compile to output Abi in separate file.

--- build/compile.js
+++ build/compile.js
@@ -2,20 +2,26 @@ const fs = require('fs')
 const path = require('path');
 const solc = require('solc')
 
-const filesBaseDir = path.join(__dirname, '..', 'contracts');
+const contractBaseDir = path.join(__dirname, '..', 'contracts');
 
 // read the contract source
-const sourcePath = path.join(filesBaseDir, 'Voting.sol');
-const sourceContract = fs.readFileSync(sourcePath).toString()
+const contractPath = path.join(contractBaseDir, 'Voting.sol');
+const contractDef = fs.readFileSync(contractPath).toString()
 
 // compile contract
-const compiledContract = solc.compile(sourceContract)
-console.log("Compiled contract:")
-console.log(compiledContract)
+const compiledContract = solc.compile(contractDef)
+const contractAbi = JSON.parse(compiledContract["contracts"][':Voting'].interface);
 
 // write out the compiled contract file as json
-const outputPath = path.join(filesBaseDir, 'Voting.json');
-fs.writeFile(outputPath, JSON.stringify(compiledContract), function(err) {
+const contractOutputPath = path.join(contractBaseDir, 'Voting.json');
+fs.writeFile(contractOutputPath, JSON.stringify(compiledContract), function(err) {
   if (err) console.log(err)
   else console.log("Successfully written compiled contract to file")
+})
+
+// write out the contract ABI as json (to be shared with all involved in the contract)
+const contractAbiOutputPath = path.join(contractBaseDir, 'Voting-Interface.json');
+fs.writeFile(contractAbiOutputPath, JSON.stringify(contractAbi), function(err) {
+  if (err) console.log(err)
+  else console.log("Successfully written compiled contract ABI to file")
 })
